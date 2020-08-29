8d9060f7c42d2de9bdebfbf4b08da26cdc19aabd
龚箭 <15031617367@163.com>
2020-08-29 20:07:15 +0800

    待优化

--- src/assets/video/index.js
+++ src/assets/video/index.js
@@ -1,4 +1,5 @@
-hwplayerloaded(() => {
+const createvideoFun = ()=>{
+  hwplayerloaded(() => {
     var player = new HWPlayer("pagesVideo", {
       controls: true,
       userId: "playerDemo01",
@@ -8,6 +9,10 @@ hwplayerloaded(() => {
       playbackRates: [0.5, 1, 1.5, 2],
     });
     player.src(
-      "https://35.cdn-vod.huaweicloud.com/asset/ba4f5df688f4ed6f569470d688ec4a22/c5d8003cb1d108035d3a902adb2bc5cc.mp4"
+    "https://35.cdn-vod.huaweicloud.com/asset/ba4f5df688f4ed6f569470d688ec4a22/c5d8003cb1d108035d3a902adb2bc5cc.mp4"
     );
-});
+  });
+}
+
+
+export default  createvideoFun
